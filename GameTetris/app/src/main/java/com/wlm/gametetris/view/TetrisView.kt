48d08c945875ceecc9bca84c9819c295f9923b14
wlm <1062397463@qq.com>
2020-09-10 19:13:35 +0800

    11

--- GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView.kt
+++ GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView.kt
@@ -20,16 +20,17 @@ class TetrisView : View {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     init {
+        bgPaint.style = Paint.Style.STROKE
         bgPaint.color = ContextCompat.getColor(context, R.color.dark_grey)
-        bgPaint.strokeWidth = context.resources.getDimension(R.dimen.dp2)
+        bgPaint.strokeWidth = context.resources.getDimension(R.dimen.dp1)
 
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val width =
-            measureSpace(widthMeasureSpec, context.resources.getDimension(R.dimen.dp250).toInt())
+            measureSpace(widthMeasureSpec, context.resources.getDimension(R.dimen.dp240).toInt())
         val height =
-            measureSpace(heightMeasureSpec, context.resources.getDimension(R.dimen.dp510).toInt())
+            measureSpace(heightMeasureSpec, context.resources.getDimension(R.dimen.dp480).toInt())
         setMeasuredDimension(width, height)
     }
 
@@ -45,8 +46,8 @@ class TetrisView : View {
                 canvas?.drawRoundRect(
                     startX + j * blockWidth, startY + i * blockWidth,
                     startX + (j + 1) * blockWidth, startY + (i + 1) * blockWidth,
-                    context.resources.getDimension(R.dimen.dp2),
-                    context.resources.getDimension(R.dimen.dp2),
+                    context.resources.getDimension(R.dimen.dp3),
+                    context.resources.getDimension(R.dimen.dp3),
                     bgPaint
                 )
             }
