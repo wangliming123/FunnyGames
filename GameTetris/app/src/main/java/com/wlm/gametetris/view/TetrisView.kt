39be28bcbb6bc54827767122999c611e232b9223
wlm <1062397463@qq.com>
2020-09-18 15:36:05 +0800

    3

--- GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView.kt
+++ GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView.kt
@@ -7,14 +7,22 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.wlm.gametetris.R
+import com.wlm.gametetris.TetrisShape
 import com.wlm.gametetris.ext.measureSpace
 
 class TetrisView : View {
 
-    var blockWidth = context.resources.getDimension(R.dimen.dp20)
+    var blockWidth = context.resources.getDimension(R.dimen.dp24)
+
+    private val blockArray = Array(20) {IntArray(10) { 0 }}
+
 
     //背景网格画笔
     private val bgPaint: Paint = Paint()
+    private val blockPaint: Paint = Paint()
+
+    private var curShape = TetrisShape()
+    private var nextShape = TetrisShape()
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -24,6 +32,9 @@ class TetrisView : View {
         bgPaint.color = ContextCompat.getColor(context, R.color.dark_grey)
         bgPaint.strokeWidth = context.resources.getDimension(R.dimen.dp1)
 
+        blockPaint.style = Paint.Style.FILL
+        blockPaint.color = ContextCompat.getColor(context, R.color.green)
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -31,26 +42,36 @@ class TetrisView : View {
             measureSpace(widthMeasureSpec, context.resources.getDimension(R.dimen.dp240).toInt())
         val height =
             measureSpace(heightMeasureSpec, context.resources.getDimension(R.dimen.dp480).toInt())
+
+        blockWidth = if (width * 2 <= height) {
+            width.toFloat() / 10
+        } else {
+            height.toFloat() / 20
+        }
         setMeasuredDimension(width, height)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        val xNum = (width / blockWidth).toInt()
-        val yNum = (height / blockWidth).toInt()
-        val startX = (width - xNum * blockWidth) / 2
-        val startY = (height - yNum * blockWidth) / 2
-        for (i in 0 until yNum) {
-            for (j in 0 until xNum) {
-                canvas?.drawRoundRect(
-                    startX + j * blockWidth, startY + i * blockWidth,
-                    startX + (j + 1) * blockWidth, startY + (i + 1) * blockWidth,
+        blockArray.forEachIndexed { i, intArray ->
+            intArray.forEachIndexed { j, num ->
+                canvas?.drawRoundRect(j * blockWidth, i * blockWidth,
+                    (j + 1) * blockWidth, (i + 1) * blockWidth,
                     context.resources.getDimension(R.dimen.dp3),
                     context.resources.getDimension(R.dimen.dp3),
                     bgPaint
                 )
+                if (num == 1) {
+                    canvas?.drawRoundRect(j * blockWidth, i * blockWidth,
+                        (j + 1) * blockWidth, (i + 1) * blockWidth,
+                        context.resources.getDimension(R.dimen.dp3),
+                        context.resources.getDimension(R.dimen.dp3),
+                        blockPaint
+                    )
+                }
             }
         }
+
     }
 }
--- GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView2.kt
+++ GameTetris/app/src/main/java/com/wlm/gametetris/view/TetrisView2.kt
@@ -15,10 +15,13 @@ class TetrisView2(context: Context) : View(context) {
     var buttonTextSize = context.resources.getDimension(R.dimen.sp12)
     var gradeTextSize = context.resources.getDimension(R.dimen.sp16)
 
+    private val blockArray = arrayOf(IntArray(20) { 0 })
+
     //背景网格画笔
     private val bgPaint: Paint = Paint()
-    private val gradeTextPaint: Paint = Paint()
-    private val buttonTextPaint: Paint = Paint()
+    private val whiteGradePaint = Paint()
+    private val gradeTextPaint = Paint()
+    private val buttonTextPaint = Paint()
 
     init {
         bgPaint.style = Paint.Style.STROKE
@@ -29,16 +32,21 @@ class TetrisView2(context: Context) : View(context) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val width =
-            measureSpace(widthMeasureSpec, context.resources.getDimension(R.dimen.dp340).toInt())
+            measureSpace(widthMeasureSpec, context.resources.getDimension(R.dimen.dp240).toInt())
         val height =
-            measureSpace(heightMeasureSpec, context.resources.getDimension(R.dimen.dp600).toInt())
+            measureSpace(heightMeasureSpec, context.resources.getDimension(R.dimen.dp480).toInt())
         if (width / 17 < height / 27) {
             blockWidth = width / 17
         } else {
             blockWidth = height / 27
         }
-        buttonTextSize = blockWidth * 0.7f
-        gradeTextSize = blockWidth * 0.5f
+        gradeTextSize = blockWidth * 0.7f
+        whiteGradePaint.textSize = buttonTextSize
+        gradeTextPaint.textSize = buttonTextSize
+        buttonTextSize = blockWidth * 0.5f
+        buttonTextPaint.textSize = buttonTextSize
+
+
         setMeasuredDimension(width, height)
     }
 
